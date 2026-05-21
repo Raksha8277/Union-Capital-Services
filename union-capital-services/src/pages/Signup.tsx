bf3fc94b7f246b17ae85893e8da49875b9e6ed5e
Raksha8277 <nbraksha@gmail.com>
2026-05-21 14:12:27 +0530

    updated project

--- union-capital-services/src/pages/Signup.tsx
+++ union-capital-services/src/pages/Signup.tsx
@@ -24,7 +24,7 @@ const Signup = () => {
 
     try {
       const res = await axios.post(
-        'http://localhost:5000/api/auth/signup',
+        'https://union-capital-services.onrender.com/api/auth/signup',
         {
           name,
           email,
